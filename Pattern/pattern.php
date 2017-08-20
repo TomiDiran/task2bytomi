e885513644207734f169971764c340393e05b94e
TomiDiran <ogundirantomi@gmail.com>
2017-08-20 14:21:52 +0100

    Tomi1
    PHP script to display pattern

---
+++ Pattern/pattern.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pattern</title>
+</head>
+<body>
+	<?php
+
+	for ($i=0; $i < 6 ; $i++) { 
+		for ($j=0; $j < $i ; $j++) { 
+			echo " * ";
+		}
+		echo " <br /> ";
+	}
+
+	?>
+
+</body>
+</html>
